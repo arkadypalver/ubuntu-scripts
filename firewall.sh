d14cb8238acacac97fcdc292154fe28a56326c14
Arkady Palver <arkadypalver@gmail.com>
2020-10-27 16:15:13 +0100

    Add files via upload

--- firewall.sh
+++ firewall.sh
@@ -6,13 +6,17 @@ sudo apt install -y ufw
 
 sudo ufw enable
 
-sudo ufw default deny incoming && sudo ufw default deny outgoing
+sudo ufw default deny incoming && sudo ufw default allow outgoing
 
-sudo ufw allow out 25,53,80,110,443,123/tcp
+# sudo ufw allow out 25,53,110,443,123/tcp
 
-sudo ufw allow out 53,67,68,123/udp
+# sudo ufw allow out 53,67,68,123,1900/udp
+# chromecast rules
+# sudo ufw allow from 192.168.170.0/24 to any port 32768:61000 proto udp
+#
+# sudo ufw allow from 192.168.170.0/24 to any port 1900 proto udp
 
-sudo ufw allow out 6969/tcp
+sudo ufw limit ssh
 
 sudo ufw allow from 192.168.170.0/24 to any port 22
 

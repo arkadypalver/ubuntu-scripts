5157d0d1accb689b622bdf092b6210846c9f4bf0
Arkady Palver <arkadypalver@gmail.com>
2020-10-27 16:14:33 +0100

    Delete ufwfirewall.sh

--- ufwfirewall.sh
+++
@@ -1,5 +0,0 @@
-sudo ufw default deny incoming && sudo ufw default deny outgoing
-sudo ufw allow out 25,53,80,110,443,51413/tcp
-sudo ufw allow out 53,67,68,51413/udp
-sudo ufw allow out 6969/tcp
-sudo ufw disable && sudo ufw enable
